d35cc82920064410479cc9a8f02d4c7a4d199627
ehachi <ehajianto@gmail.com>
2023-03-15 06:17:20 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
+    validate :sufficiently_clickbait_title
+  
+    private
+  
+    def sufficiently_clickbait_title
+      unless title&.match?(/(Won't Believe|Secret|Top \d+|Guess)/)
+        errors.add(:title, "must be sufficiently clickbait-y")
+      end
+    end
+  end
